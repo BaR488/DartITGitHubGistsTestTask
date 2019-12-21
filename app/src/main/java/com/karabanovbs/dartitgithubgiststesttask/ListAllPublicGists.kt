b5efd7ed08c3a2efb28837d3cbbf487e575f1dd8
Boris Karabanov <karabanovbswork@gmail.com>
2019-12-22 00:45:03 +0400

    Initial

---
+++ app/src/main/java/com/karabanovbs/dartitgithubgiststesttask/ListAllPublicGists.kt
@@ -0,0 +1,12 @@
+package com.karabanovbs.dartitgithubgiststesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListAllPublicGists : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_all_public_gists)
+    }
+}
