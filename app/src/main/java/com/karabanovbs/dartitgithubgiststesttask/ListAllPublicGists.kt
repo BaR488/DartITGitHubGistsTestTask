e8b050c8b186068a003d33f06045698d16068295
Boris Karabanov <karabanovbswork@gmail.com>
2019-12-22 02:34:03 +0400

    Add simple list

--- app/src/main/java/com/karabanovbs/dartitgithubgiststesttask/ListAllPublicGists.kt
+++ app/src/main/java/com/karabanovbs/dartitgithubgiststesttask/ListAllPublicGists.kt
@@ -2,11 +2,37 @@ package com.karabanovbs.dartitgithubgiststesttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.karabanovbs.dartitgithubgiststesttask.adapters.GistsAdapter
 
 class ListAllPublicGists : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_all_public_gists)
+
+        viewManager = LinearLayoutManager(this)
+
+        val myDataset = Array(5) { i -> (i * i).toString() }
+
+        viewAdapter = GistsAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
 }
